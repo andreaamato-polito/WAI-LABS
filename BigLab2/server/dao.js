2d107986b94b6433bf3bd47113afbd777b871764
Andrea Amato s236053 <s236053@studenti.polito.it>
2021-05-08 19:13:23 +0200

    complete week1 biglab2

---
+++ BigLab2/server/dao.js
@@ -0,0 +1,206 @@
+'use strict';
+// Data Access Object (DAO)
+
+const sqlite = require('sqlite3');
+const dayjs = require('dayjs')
+
+
+// open the database
+const db = new sqlite.Database('tasks.db', (err) => {
+    if (err) throw err;
+});
+
+// get all tasks
+exports.listTasks = () => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT * FROM tasks';
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            const tasks = rows.map((t) => ({
+                id: t.id,
+                description: t.description,
+                important: t.important,
+                private: t.private,
+                deadline: t.deadline,
+                completed: t.completed,
+                user: t.user
+            }));
+            resolve(tasks)
+        });
+    });
+};
+
+
+exports.importantTasks = () => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT * FROM tasks WHERE important=1';
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            const tasks = rows.map((t) => ({
+                id: t.id,
+                description: t.description,
+                important: t.important,
+                private: t.private,
+                deadline: t.deadline,
+                completed: t.completed,
+                user: t.user
+            }));
+            resolve(tasks)
+        });
+    });
+};
+
+
+
+exports.privateTasks = () => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT * FROM tasks WHERE private=1';
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            const tasks = rows.map((t) => ({
+                id: t.id,
+                description: t.description,
+                important: t.important,
+                private: t.private,
+                deadline: t.deadline,
+                completed: t.completed,
+                user: t.user
+            }));
+            resolve(tasks)
+        });
+    });
+};
+
+exports.todayTasks = () => {
+    return new Promise((resolve, reject) => {
+        const date = dayjs().format('YYYY-MM-DD');
+        const sql = "SELECT * FROM tasks WHERE deadline=?";
+        db.all(sql, date, (err, rows) =>{
+            if(err){
+                reject(err);
+                return;
+            }
+            const tasks = rows.map((t) =>({
+                id: t.id,
+                description: t.description,
+                important: t.important,
+                private: t.private,
+                deadline: t.deadline,
+                completed: t.completed,
+                user: t.user
+            }));
+            resolve(tasks);
+        });
+    });
+};
+
+exports.next7DaysTasks = () => {
+    return new Promise((resolve, reject) => {
+        const today = dayjs().format('YYYY-MM-DD');
+        const nextWeek = dayjs().add(7, 'day').format('YYYY-MM-DD');
+        const sql = "SELECT * FROM tasks WHERE deadline>? AND deadline <=?";
+        db.all(sql, [today, nextWeek], (err, rows) =>{
+            if(err){
+                reject(err);
+                return;
+            }
+            const tasks = rows.map((t) =>({
+                id: t.id,
+                description: t.description,
+                important: t.important,
+                private: t.private,
+                deadline: t.deadline,
+                completed: t.completed,
+                user: t.user
+            }));
+            resolve(tasks);
+        });
+    });
+};
+
+exports.getTask = (id) => {
+    return new Promise((resolve, reject) =>{
+        const sql = 'SELECT * FROM tasks where id=?';
+        db.get(sql, [id], (err, row) =>{
+            if (err){
+                reject(err);
+                return;
+            }
+            if (row == undefined){
+                reject({error: 'Task not found.'});
+            } else {
+                const task = {
+                    id: row.id,
+                    description: row.description,
+                    important: row.important,
+                    private: row.private,
+                    deadline: row.deadline,
+                    completed: row.completed,
+                    user: row.user
+                };
+                resolve(task);
+            }
+        });
+    });
+};
+
+exports.createTask = (task) => {
+    return new Promise((resolve, reject) =>{
+        const sql = 'INSERT INTO tasks(id, description, important, private, deadline, completed, user) VALUES(?, ?, ?, ?, DATE(?), ?, ?)';
+        db.run(sql, [task.id, task.description, task.important, task.private, task.deadline, task.completed, task.user], function (err){
+            if (err){
+                reject(err);
+                return;
+            }
+            resolve(this.lastID)
+        });
+    });
+};
+
+exports.deleteTask = (id) => {
+    return new Promise((resolve, reject) =>{
+        const sql = 'DELETE FROM tasks WHERE id=?';
+        db.run(sql, [id], (err) =>{
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(null);
+        });
+    });
+};
+
+exports.updateTask = (task) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'UPDATE tasks SET description=?, important=?, private=?, deadline=DATE(?), completed=?, user=? WHERE id=?';
+        db.run(sql, [task.description, task.important, task.private, task.deadline, task.completed, task.user, task.id], function (err) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(this.lastID);
+        });
+    });
+};
+
+exports.markTask = (task) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'UPDATE tasks SET completed=? WHERE id=?';
+        db.run(sql, [!task.completed, task.id], function (err) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(this.lastID);
+        });
+    });
+};
--- BigLab2/server/server.js
+++ BigLab2/server/server.js
@@ -2,14 +2,122 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const dao = require('./dao');
 
-app = express();
+let app = new express();
 const PORT = 3000;
 
 app.use(morgan('dev'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello world, from your server');
+app.get('/api/tasks', (req, res) => {
+    dao.listTasks()
+        .then((tasks) => { res.json(tasks); })
+        .catch((error) => { res.status(500).json(error); });
 });
 
-app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}/`));
+app.get('/api/tasks/important', (req, res) => {
+    dao.importantTasks()
+        .then((tasks) => { res.json(tasks); })
+        .catch((error) => { res.status(500).json(error); });
+});
+
+app.get('/api/tasks/private', (req, res) => {
+    dao.privateTasks()
+        .then((tasks) => { res.json(tasks); })
+        .catch((error) => { res.status(500).json(error); });
+});
+
+app.get('/api/tasks/today', (req, res) => {
+    dao.todayTasks()
+        .then((tasks) => { res.json(tasks); })
+        .catch((error) => { res.status(500).json(error) });
+});
+
+app.get('/api/tasks/next7Days', (req, res) => {
+    dao.next7DaysTasks()
+        .then((tasks) => { res.json(tasks); })
+        .catch((error) => { res.status(500).json(error); });
+});
+
+app.get('/api/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    dao.getTask(id)
+        .then((task) => { res.json(task); })
+        .catch((error) => { res.status(500).json(error); });
+});
+
+app.post('/api/tasks', async (req, res) => {
+    let id = req.body.id;
+    let description = req.body.description;
+    let important = req.body.important;
+    let prvt = req.body.private;
+    let deadline = req.body.deadline;
+    let completed = req.body.completed;
+    let user = req.body.user;
+
+    try {
+        await dao.createTask({
+            id: id,
+            description: description,
+            important: important,
+            private: prvt,
+            deadline: deadline,
+            completed: completed,
+            user: user
+        });
+        res.end();
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+app.delete('/api/tasks/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        await dao.deleteTask(id);
+        res.end();
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+app.put('/api/tasks/:id', async (req, res) => {
+    const id = req.params.id;
+    let description = req.body.description;
+    let important = req.body.important;
+    let prvt = req.body.private;
+    let deadline = req.body.deadline;
+    let completed = req.body.completed;
+    let user = req.body.user;
+
+    try {
+        await dao.updateTask({
+            id: id,
+            description: description,
+            important: important,
+            private: prvt,
+            deadline: deadline,
+            completed: completed,
+            user: user
+        });
+        res.end();
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+app.put('/api/tasks/completed/:id', async (req, res) => {
+    const id = req.params.id;
+    try { 
+        let task = await dao.getTask(id);
+        await dao.markTask(task);
+        res.end();
+    }catch (error){
+        res.status(500).json(error);
+    }
+});
+
+
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`));
